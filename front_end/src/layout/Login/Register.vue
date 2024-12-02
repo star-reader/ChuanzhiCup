3a1263b223e683625c307abaa10f764923678d8b
star-reader <3084895416@qq.com>
2024-12-02 14:05:10 +0800

    add login page

---
+++ front_end/src/layout/Login/Register.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="register-container">
+    <van-field v-model="username" placeholder="用户名" />
+    <van-field v-model="password" type="password" placeholder="密码" />
+    <van-field v-model="confirmPassword" type="password" placeholder="确认密码" />
+    <van-button type="primary" @click="handleRegister">注册</van-button>
+    <van-button @click="$router.push('/login')">登录</van-button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  data() {
+    return {
+      username: '',
+      password: '',
+      confirmPassword: ''
+    };
+  },
+  methods: {
+    handleRegister() {
+      // 注册逻辑
+      console.log('注册:', this.username, this.password);
+    }
+  }
+});
+</script>
+
+<style scoped>
+.register-container {
+  padding: 20px;
+}
+</style> 
--- front_end/src/router/index.ts
+++ front_end/src/router/index.ts
@@ -1,15 +1,29 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Login from '../layout/Login/Login.vue'
+import Register from '../layout/Login/Register.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView,
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: Register
+  },
+]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-  ],
+  history: createWebHistory(),
+  routes
 })
 
 export default router
